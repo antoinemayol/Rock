274f06448164673d228242b00a7f26c66c47cb10
Antoine Mayol <antoinemayol@yahoo.com>
2022-09-12 22:06:15 +0200

    Connexes components - IN PROGRESS
    Connexes components - function to discern different part of a bin matrix

--- Traitement_image/composantes_connexes.c
+++ Traitement_image/composantes_connexes.c
@@ -1,49 +1,72 @@
 #include <stdio.h>
 #include <string.h>
 
-void print_mat(int *matrix, int length, int width)
+void print_mat(int lenght, int width, int matrix[lenght][width]);
+void connected_components(int l, int w, int matrix[l][w], int output[l][w]);
+void get_partitioned_matrix(int l, int w,int matrix[l][w], int output[l][w]);
+
+int main()
+{
+    int lenght= 6;
+    int width= 6;
+    int input[6][6] = {
+        {0,0,0,0,0,0},
+        {0,1,0,1,0,0},
+        {0,1,1,1,0,0},
+        {0,0,0,0,0,0},
+        {0,0,1,1,1,0},
+        {0,0,1,1,1,0}};
+    int out[6][6] ={0};
+    connected_components(lenght,width,input,out);
+    print_mat(lenght, width, out);
+    return 0;
+}
+
+void print_mat(int lenght, int width, int matrix[lenght][width])
 {
 	int i;
-    for(i = 0 ; i < length ; ++i)
+    for(i = 0 ; i < lenght ; ++i)
     {
-    	int j;	
+    	int j;
     	for (int j = 0; j < width; ++j)
     	{
-    		printf("%d ", i[matrix][j]);//to fix
+    		printf("%d ", i[matrix][j]);
     	}
-    	printf("\n");	
+    	printf("\n");
     }
     printf("\n");
 }
 
-
-int main()
+void connected_components(int l, int w, int matrix[l][w], int output[l][w])
 {
-	/*printf("lol\n");
-	int rows = 5;
-	int columns = 5;
-
-	int mat[5][5]=	{{0,0,0,0,0},
-					{0,1,0,0,0},
-					{0,1,1,1,0},
-					{0,0,1,1,0},
-					{0,0,0,0,0}};
-
-
-	print_mat(rows, columns, mat);
-	*/
-	int lenght = 6;
-	int width = 8;
-
-	int visited[lenght][width];
-	for (int i = 0; i < lenght ; ++i)
-	{
-		for (int j = 0; j < width; ++j)
-		{
-			visited[i][j] = 0;
-		}
-	}
-
-	return 0;
+    get_partitioned_matrix(l,w,matrix,output);
 }
 
+void get_partitioned_matrix(int l, int w,int matrix[l][w], int output[l][w])
+{
+    int part_number = 0;
+
+    for(int i = 0; i < l; ++i)
+    {
+        for(int j = 0; j < w; ++j)
+        {
+
+            if(matrix[i][j] == 1)
+            {
+                if(j > 0 && matrix[i][j-1] == 1 )
+                {
+                    output[i][j] = output[i][j-1];
+                }
+                else if( i > 0 && matrix[i-1][j] == 1 )
+                {
+                    output[i][j] = output[i-1][j];
+                }
+                else
+                {
+                    ++part_number;
+                    output[i][j] = part_number;
+                }
+            }
+        }
+    }
+}
