c7abf53494a0443da38f82fd3b76cc934372e153
Antoine Mayol <antoinemayol@yahoo.com>
2022-09-13 10:56:01 +0200

    Connexes components - IN PROGRESS
    Adding function apply_equivalence to fix subpartitionning. Comment apply_equivalence at the end of get_partionned_matrix to see diff.

--- Traitement_image/composantes_connexes.c
+++ Traitement_image/composantes_connexes.c
@@ -4,6 +4,7 @@
 void print_mat(int lenght, int width, int matrix[lenght][width]);
 void connected_components(int l, int w, int matrix[l][w], int output[l][w]);
 void get_partitioned_matrix(int l, int w,int matrix[l][w], int output[l][w]);
+void apply_equivalence(int l, int w, int output[l][w], int equivalence[1000]);
 
 int main()
 {
@@ -14,8 +15,8 @@ int main()
         {0,1,0,1,0,0},
         {0,1,1,1,0,0},
         {0,0,0,0,0,0},
-        {0,0,1,1,1,0},
-        {0,0,1,1,1,0}};
+        {0,0,1,0,1,0},
+        {0,1,1,1,1,0}};
     int out[6][6] ={0};
     connected_components(lenght,width,input,out);
     print_mat(lenght, width, out);
@@ -45,27 +46,52 @@ void connected_components(int l, int w, int matrix[l][w], int output[l][w])
 void get_partitioned_matrix(int l, int w,int matrix[l][w], int output[l][w])
 {
     int part_number = 0;
-
+    int equivalence[1000] = {0};
     for(int i = 0; i < l; ++i)
     {
         for(int j = 0; j < w; ++j)
         {
-
             if(matrix[i][j] == 1)
             {
                 if(j > 0 && matrix[i][j-1] == 1 )
                 {
-                    output[i][j] = output[i][j-1];
+                    if(i>0&&matrix[i-1][j]==1 && output[i-1][j]<output[i][j-1])
+                    {
+                        equivalence[output[i][j-1]] = output[i-1][j];
+                        output[i][j] = output[i-1][j];
+                    }
+                    else
+                    {
+                        if(i>0&&matrix[i-1][j]==1)
+                        {
+                            equivalence[output[i-1][j]] = output[i][j-1];
+                        }
+                        output[i][j] = output[i][j-1];
+                    }
                 }
                 else if( i > 0 && matrix[i-1][j] == 1 )
-                {
                     output[i][j] = output[i-1][j];
-                }
                 else
                 {
                     ++part_number;
                     output[i][j] = part_number;
+                    equivalence[part_number] = part_number;
                 }
+            }
+        }
+    }
+    apply_equivalence(l,w,output,equivalence);
+}
+
+void apply_equivalence(int l, int w, int output[l][w], int equivalence[1000])
+{
+    for(int i = 0; i < l; ++i)
+    {
+        for(int j = 0; j < w; ++j)
+        {
+            if(output[i][j] != equivalence[output[i][j]])
+            {
+                output[i][j] = equivalence[output[i][j]];
             }
         }
     }
