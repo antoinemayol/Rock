1f2db45fd01932dea974a8f4a6f94488c89c5a3d
Antoine Mayol <antoinemayol@yahoo.com>
2022-09-17 18:21:48 +0200

    Cleaning hierarchy & script structure
    check the modif to know the right way to document your code

--- Detection_grille_case/components_connexes.c
+++ Detection_grille_case/components_connexes.c
@@ -1,50 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 
-void print_mat(int lenght, int width, int matrix[lenght][width]);
-void connected_components(int l, int w, int matrix[l][w], int output[l][w]);
-void get_partitioned_matrix(int l, int w,int matrix[l][w], int output[l][w]);
-void apply_equivalence(int l, int w, int output[l][w], int equivalence[1000]);
-
-int main()
-{
-    int lenght= 6;
-    int width= 6;
-    int input[6][6] = {
-        {0,0,0,0,0,0},
-        {0,1,0,1,0,0},
-        {0,1,1,1,0,0},
-        {0,0,0,0,0,0},
-        {0,0,1,0,1,0},
-        {0,1,1,1,1,0}};
-    int out[6][6] ={0};
-    connected_components(lenght,width,input,out);
-    print_mat(lenght, width, out);
-    return 0;
-}
-
 void print_mat(int lenght, int width, int matrix[lenght][width])
 {
-	int i;
-    for(i = 0 ; i < lenght ; ++i)
+    /* Print a matrix
+     * ARGS:
+     *  -lenght (int) : lenght of the matrix
+     *  -width (int) : width of the matrix
+     *  -matrix (int[]) : the matrix to print
+     * OUT:
+     *  none
+     */
+    for(int i = 0 ; i < lenght ; ++i)
     {
-    	int j;
     	for (int j = 0; j < width; ++j)
-    	{
     		printf("%d ", i[matrix][j]);
-    	}
     	printf("\n");
     }
     printf("\n");
 }
 
-void connected_components(int l, int w, int matrix[l][w], int output[l][w])
+void apply_equivalence(int l, int w, int output[l][w], int equivalence[1000])
 {
-    get_partitioned_matrix(l,w,matrix,output);
+    /* Apply equivalence on the matrix
+     * ARGS:
+     *  -lenght (int) : lenght of the matrix
+     *  -width (int) : width of the matrix
+     *  -matrix (int[]) : the matrix to modify
+     *  -equivalence (int[]) : array of equivalence
+     * OUT:
+     *  none
+     */
+    for(int i = 0; i < l; ++i)
+        for(int j = 0; j < w; ++j)
+            if(output[i][j] != equivalence[output[i][j]])
+                output[i][j] = equivalence[output[i][j]];
 }
 
 void get_partitioned_matrix(int l, int w,int matrix[l][w], int output[l][w])
 {
+    /* Reconize different part of the picture
+     * ARGS:
+     *  -lenght (int) : lenght of the matrix
+     *  -width (int) : width of the matrix
+     *  -matrix (int[]) : the matrix to analyse
+     *  -output (int[]) : the output matrix
+     * OUT:
+     *  none
+     */
     int part_number = 0;
     int equivalence[1000] = {0};
     for(int i = 0; i < l; ++i)
@@ -63,9 +66,7 @@ void get_partitioned_matrix(int l, int w,int matrix[l][w], int output[l][w])
                     else
                     {
                         if(i>0&&matrix[i-1][j]==1)
-                        {
                             equivalence[output[i-1][j]] = output[i][j-1];
-                        }
                         output[i][j] = output[i][j-1];
                     }
                 }
@@ -83,16 +84,16 @@ void get_partitioned_matrix(int l, int w,int matrix[l][w], int output[l][w])
     apply_equivalence(l,w,output,equivalence);
 }
 
-void apply_equivalence(int l, int w, int output[l][w], int equivalence[1000])
+void connected_components(int l, int w, int matrix[l][w], int output[l][w])
 {
-    for(int i = 0; i < l; ++i)
-    {
-        for(int j = 0; j < w; ++j)
-        {
-            if(output[i][j] != equivalence[output[i][j]])
-            {
-                output[i][j] = equivalence[output[i][j]];
-            }
-        }
-    }
+    /* Reconize the sudoku on the picture
+     * ARGS:
+     *  -lenght (int) : lenght of the matrix
+     *  -width (int) : width of the matrix
+     *  -matrix (int[]) : the initial binary matrix from the picture
+     *  -ouput (int[]) : the sudoku matrix
+     * OUT:
+     *  none
+     */
+    get_partitioned_matrix(l,w,matrix,output);
 }
---
+++ Detection_grille_case/components_connexes.h
@@ -0,0 +1,9 @@
+#ifndef COMPONENTS_CONNEXES_H
+#define COMPONENTS_CONNEXES_H
+
+void print_mat(int lenght, int width, int matrix[lenght][width]);
+void connected_components(int l, int w, int matrix[l][w], int output[l][w]);
+void get_partitioned_matrix(int l, int w,int matrix[l][w], int output[l][w]);
+void apply_equivalence(int l, int w, int output[l][w], int equivalence[1000]);
+
+#endif
