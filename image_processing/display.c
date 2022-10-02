5c91d9a02b81a75cf8cefbfa43949acc628f8440
Octopus <thybalt.marschutz@outlook.com>
2022-10-02 18:38:31 +0200

    display and grayscale working

---
+++ image_processing/display.c
@@ -0,0 +1,14 @@
+#include <err.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+// Updates the display.
+//
+// renderer: Renderer to draw on.
+// texture: Texture that contains the image.
+void draw(SDL_Renderer* renderer, SDL_Texture* texture)
+{
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+}
+
