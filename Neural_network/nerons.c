ee69c055ac4d2796c4f4c1fc57031a1984d7f2fd
panpanDKC <97619055+panpanDKC@users.noreply.github.com>
2022-10-14 14:14:19 +0200

    debut parsing pour neurones

--- Neural_network/nerons.c
+++ Neural_network/nerons.c
@@ -8,7 +8,6 @@
 //Retourne un neurones
 node give_node(double wht, double _x1, double _x2, double _w1, double _w2, char* _name)
 {
-	printf("debut neu\n");
 	node n;
 	n.weight = wht;
 	n.x1 = _x1;
@@ -33,21 +32,18 @@ void concat_path(char* res,char* s1, char* s2, char* s3, int maxpath)
 	for(size_t i = 0; k < maxpath && s1[i] != '\0'; i++)
 	{
 		res[k]=s1[i];
-		printf("%c",res[i]);
 		k+=1;
 	}
 
 	for(size_t i = 0; k < maxpath && s2[i] != '\0'; i++)
 	{
 		res[k]=s2[i];
-		printf("%c",res[i]);
 		k+=1;
 	}
 
 	for(size_t i = 0; k < maxpath && s3[i] != '\0'; i++)
 	{
 		res[k]=s3[i];
-		printf("%c",res[i]);
 		k+=1;
 	}
 
@@ -56,10 +52,7 @@ void concat_path(char* res,char* s1, char* s2, char* s3, int maxpath)
 //mets à jours les valeurs dans le fichier du noeud n
 void refresh(node *n)
 {
-	printf(" | before open\n");
-	printf("%s",n->path);
 	FILE *f = fopen(n->path,"w");
-	printf("after open \n");
 	if(f == NULL)
    	{
 		errx(1,"can't open file !");
@@ -72,4 +65,25 @@ void refresh(node *n)
 	fprintf(f,"W1 : %f \n",n->w1);
 	fprintf(f,"W2 : %f \n",n->w2);
 	fclose(f);
+}
+
+void fromfile(char* path)
+{
+	//node n;
+	FILE *f = fopen(path,"r");
+	if(f == NULL)
+   	{
+		errx(1,"can't open file !");
+	}
+		
+	char *line =NULL;
+	size_t len = 0;
+
+	while(getline(&line, &len, f) != -1)
+	{
+		printf("line length : %zd\n",strlen(line));
+	}
+
+	fclose(f);
+	free(line);
 }
