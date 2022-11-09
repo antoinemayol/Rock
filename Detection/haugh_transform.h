bcc2d3f79362b6ab5635f92f4a55029e60b1f597
Antoine Mayol <antoinemayol@yahoo.com>
2022-11-09 18:01:41 +0100

    Cases detection done
    We're all in the same boat, most of us.
    
    So he wrote a nice thing in the book, and I thought you'd like to share it.
    
    Keep the laughter coming!

--- Detection/haugh_transform.h
+++ Detection/haugh_transform.h
@@ -3,8 +3,13 @@
 
 int *hough_transform(int h, int w, int *mat, int *hough_val);
 void print_graph(int l, int w, int *matrix);
+void print_case(int **coo, int* mat, int width);
 int get_max_hough_occ(int l, int w, int *hough);
-void get_equation(int roh, int phi, int *hough_val, int *equation);
-void hough_traitement(int l, int w, int *mat);
+int** get_equation(int roh, int phi, int *hough_val, int *equation);
+int** hough_traitement(int l, int w, int *mat);
+int is_val_in_arr(int x, int* arr,int len);
+int** extract_intersection(int* horizontal_equ, int* vertical_equ, int lenh, int lenv);
+int deg_approximation(int deg,int biais, int cible);
+void stock_cases(int** coo, int* matrix, int width, int** cases);
 
 #endif
