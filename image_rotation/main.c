364f75bfdd86e537dc20bd1feff65cc750270b1a
Clery Pelvillain <clery.pelvillain@epita.fr>
2022-11-09 20:33:07 +0100

    update image rot

--- image_rotation/main.c
+++ image_rotation/main.c
@@ -4,12 +4,19 @@
 
 #include "rotation.h"
 
+//HEADER
+//
+void draw(SDL_Renderer* renderer, SDL_Texture* texture);
+void draw2(SDL_Surface *surface);
+void event_loop(SDL_Renderer* renderer, SDL_Texture* texture,SDL_Surface* surface,float angle);
+SDL_Surface* load_image(const char* path);
 //initialisation taille du display
 //
 const int INIT_WIDTH = 640;
 const int INIT_HEIGHT = 400;
-const float INIT_ANGLE = 30;
+float INIT_ANGLE = 45;
 
+//permet de afficher ou de reaficher l'image
 void draw(SDL_Renderer* renderer, SDL_Texture* texture)
 {
     SDL_RenderCopy(renderer,texture,NULL,NULL);
@@ -29,6 +36,7 @@ void draw2(SDL_Surface *surface)
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if(renderer == NULL)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
+
    //resizing
    int w = surface->w;
    int h = surface->h;
@@ -36,11 +44,12 @@ void draw2(SDL_Surface *surface)
     
    //creating texture from image
    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
    //SDL_Texture* processed =  SDL_CreateTextureFromSurface(renderer, surface);
    SDL_FreeSurface(surface);
 
    //calling the main loop
-   event_loop(renderer, texture);
+   //event_loop(renderer, texture,surface;
    //test_loop(renderer, texture, processed);
    
    //Destroying objects
@@ -54,7 +63,7 @@ void draw2(SDL_Surface *surface)
 
 }
    
-
+//check all the different event to update our image
 void event_loop(SDL_Renderer* renderer, SDL_Texture* texture,SDL_Surface* surface,float angle)
 {
     
@@ -86,6 +95,7 @@ void event_loop(SDL_Renderer* renderer, SDL_Texture* texture,SDL_Surface* surfac
     SDL_Quit();
 }
 
+//take a new SDL_Surface from an image to use her
 SDL_Surface* load_image(const char* path)
 {
     SDL_Surface* new_surface = IMG_Load(path);
@@ -93,11 +103,38 @@ SDL_Surface* load_image(const char* path)
     return format;
 }
 
+//change the name of the new file
+char *name_filled(char* img_name)
+{
+    int i = 0;
+    for(;img_name[i] != '\0';i++);
+    char *res = malloc(sizeof(int)*i+8);
+    char *end = "Rot.bmp";
+    int y = 0;
+    for(;img_name[y]!= '\0';y++)
+    {
+        res[y]=img_name[y];
+    }
+    y++;
+    int z =0;
+    while(z < 8)
+    {
+        res[y]= end[z];
+        y++;
+        z++;
+    }
+ 
+    return res;
+
+}
+
 int main(int argc, char** argv)
 {
     // Checks the number of arguments.
     if (argc != 2)
         errx(EXIT_FAILURE, "Usage: image-file");
+
+
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
@@ -142,6 +179,8 @@ int main(int argc, char** argv)
     //event_loop(renderer,texture,surface,angle);
 
     SDL_Surface *result = SDL_RotationCentrala(surface,angle);
+
+    //char *res = name_filled(argv[1]);
     if(SDL_SaveBMP(result,"imageRot.bmp") != 0)
     {
         printf("SDL_SaveBMP failed: %s\n", SDL_GetError());
