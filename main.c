33563b95d052cc9766e420e7b3556c144dd4f327
Antoine Mayol <antoinemayol@yahoo.com>
2022-11-21 21:36:25 +0100

    Structure orga:
    starting making a way to follow

---
+++ main.c
@@ -0,0 +1,21 @@
+void main()
+{
+    /*  
+     *  1-start graphic interface
+     *  2-image selection
+     *  3-image traitment: from an image
+     *      must return a matrix of 0 and 1
+     *  4-image detection: from a matrix of 0 and 1
+     *      .grid detection
+     *      .case detection
+     *      must return all case
+     *  5-neuronal network: from all case
+     *      .found numbers associated to each cases
+     *      must return a matrix with known number and empty for missing numbers
+     *  6-sudoku solver: from a matrix of int
+     *      .resolve the sudoku
+     *      must return a none empty matrix with solve sudoku
+     *  7-resolved sudoku display
+    */
+
+}
