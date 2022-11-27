516515809a7ee01c1f2ef2d152146548e32bb077
Mathis <person.mathis@orange.fr>
2022-11-27 01:39:56 +0100

    debut de l'interface
    creation window button quit et autre event

--- interface/interface.c
+++ interface/interface.c
@@ -1,42 +1,55 @@
-#include <stdlib.h>
+#include <gtk/gtk.h>
 #include <stdio.h>
-#include "SDL.h"
- 
-SDL_Surface* affichage;
- 
-void initSDL(void);
-void attendreTouche(void);
- 
-int main(int argc, char** argv)
+
+GtkWidget *window;
+GtkLabel *label;
+
+
+
+int main (int argc, char *argv[])
 {
-  initSDL();
-  attendreTouche();
-  return EXIT_SUCCESS;
+	//Initialize gtk
+	gtk_init(&argc,&argv);
+
+	//gtk bulder
+	GtkBuilder *builder;
+
+	builder = gtk_builder_new();
+	
+	gtk_builder_add_from_file(builder,"inter.glade", NULL); //load our ui file
+	
+	window = GTK_WIDGET(gtk_builder_get_object(builder,"MyWindow"));
+	label = GTK_LABEL(gtk_builder_get_object(builder,"MyLabel"));
+
+	gtk_builder_connect_signals(builder,NULL);
+
+	g_object_unref(builder);
+															
+	
+	//show the window
+	gtk_widget_show_all(window);
+	
+	//run the main loop
+	gtk_main();
+
 }
- 
-void initSDL(void)
+
+void exit_app()
 {
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    fprintf(stderr, "Erreur à l'initialisation de la SDL : %s\n", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }
- 
-  atexit(SDL_Quit);
-  affichage = SDL_SetVideoMode(800, 600, 32, SDL_SWSURFACE);
- 
-  if (affichage == NULL) {
-    fprintf(stderr, "Impossible d'activer le mode graphique : %s\n", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }
- 
-  SDL_WM_SetCaption("Mon premier programme SDL", NULL);
+	printf("Exit function \n");
+	gtk_main_quit();
 }
- 
-void attendreTouche(void)
+
+void button_cliked()
 {
-  SDL_Event event;
- 
-  do
-    SDL_WaitEvent(&event);
-  while (event.type != SDL_QUIT && event.type != SDL_KEYDOWN);
+	printf("Button clicked\n");
+	
+	exit_app();
+
+}
+
+void button_label()
+{
+	printf("button_label_cliked\n");
+	gtk_label_set_text(label,"Love You Mate");
 }
