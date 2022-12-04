d6035782d7a35f4cb2e0d2677a06ddff3f1aec65
Mathis <person.mathis@orange.fr>
2022-12-04 16:28:36 +0100

    generation image grille fini
    génère des images de grille solver ou pas

---
+++ interface/display.c
@@ -0,0 +1,287 @@
+#include <err.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+
+
+
+int grille[9*9] = { 0, 0, 6, 2, 0, 0, 0, 8, 0 ,
+      	   		0, 0, 8, 9, 7, 0, 0, 0, 0 ,
+      	   		0, 0, 4, 8, 1, 0, 5, 0, 0 ,
+           		0, 0, 0, 0, 6, 0, 0, 0, 2 ,
+           		0, 7, 0, 0, 0, 0, 0, 3, 0 ,
+           		6, 0, 0, 0, 5, 0, 0, 0, 0 ,
+           		0, 0, 2, 0, 4, 7, 1, 0, 0 ,
+           		0, 0, 3, 0, 2, 8, 4, 0, 0 ,
+           		0, 5, 0, 0, 0, 1, 2, 0, 0 };
+
+int pos[9] = {8,112,216,324,428,532,640,744,848};
+int state = 2;
+
+// Updates the display.
+//
+// renderer: Renderer to draw on.
+// texture: Texture that contains the image.
+void draw(SDL_Renderer* renderer, SDL_Texture* texture)
+{
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+}
+
+// Event loop that calls the relevant event handler.
+// renderer: Renderer to draw on.
+// texture: Texture to display.
+void event_loop(SDL_Renderer* renderer, SDL_Texture* texture)
+{
+    SDL_Event event;
+    draw(renderer,texture);
+    while(1)
+    {
+        SDL_WaitEvent(&event);
+
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                return;
+
+            case SDL_WINDOWEVENT:
+                if(event.window.event == SDL_WINDOWEVENT_RESIZED)
+                {
+                    draw(renderer,texture);
+                }
+                break;
+        }
+    }
+}
+//______________________________________________________________________________________________________________________________
+int absentOnRow (int k, int *grid, int i)
+{
+	for(int j = 0; j < 9;j++)
+	{
+		if(grid[i*9+j] == k)
+			return 0;
+	}
+	return 1;
+}
+
+//Check if the value k are on the col
+int absentOnCol (int k, int *grid, int j) 
+{
+    for(int i = 0; i < 9;i++)
+	{
+		if(grid[i*9+j] == k)
+			return 0;
+	}
+	return 1;
+}
+
+//Check if the value k are on the square
+int absentOnBloc (int k, int *grid, int i, int j)
+{
+	i -= i%3;
+	j -= j%3;
+	for(int y = i; y < i+3 && y < 9;y++)
+	{
+		for(int z = j; z < j+3 && z< 9;z++)
+		{
+			if(grid[y*9+z] == k)
+				return 0;
+		}
+	}
+	return 1;
+}
+
+int backtracking(int *grid,int position)
+{
+		//We see if the position is at the end of the matrice
+		if (position > 80)
+				return 1;
+		
+		//We initialize the variable i and j with position
+		int i = position / 9;
+		int j = position % 9;
+		
+			
+		if(grid[i*9+j] != 0)
+		{
+			return backtracking(grid,position + 1);
+		}
+		
+		//Start the backtracking with a variable that can take all the different value in a sudoku
+		for(int k = 1; k <= 9;k++)
+		{
+			if(absentOnRow(k,grid,i) && absentOnCol(k,grid,j) && absentOnBloc(k,grid,i,j))
+			{
+				grid[i*9+j] = k;
+
+				if(backtracking(grid,position + 1))
+						return 1;
+			}
+		}
+			
+		grid[i*9+j] = 0;
+		return 0;
+}
+
+//______________________________________________________________________________________________________________________________________
+
+SDL_Texture *change_board(int *board,int state, SDL_Surface *surface_p,SDL_Renderer *renderer)
+{
+	SDL_Texture *newT;
+	SDL_Surface *surface_i;
+	SDL_Rect dstrect;
+
+	if(state == 2)
+	{
+		
+		int bb[81]; 
+		memcpy(bb, board, 81 * sizeof(int));
+		backtracking(bb,0);
+		for(int i = 0; i < 81; i++)
+		{
+			printf("val : %d\n",bb[i]);
+		}
+		int j = 0;
+		while(j < 81)
+		{
+			bb[j] -= board[j];
+
+			if(bb[j] != 0)
+			{
+				printf("pos : %d\n",j);
+				char x[2];
+			
+				x[0] = bb[j] + 48;
+				x[1] = '\0';
+			
+				char *target = malloc(sizeof(char)*30);
+
+				strcat(target,"data/num_");
+				strcat(target,x);
+				strcat(target,"g.png");
+				printf("target :%s\n",target);
+			
+				surface_i = IMG_Load(target);
+			
+				dstrect.x = pos[j%9];
+				dstrect.y = pos[j/9];
+			
+
+				printf("pos x : %d pos y : %d\n",dstrect.x,dstrect.y);
+
+				SDL_BlitSurface(surface_i,NULL,surface_p,&dstrect);
+				printf("pas de segfault \n");	
+				newT = SDL_CreateTextureFromSurface(renderer,surface_p);
+				free(target);
+
+			}
+			j+=1;
+		}
+	}
+	
+	int i = 0;
+	
+	while(i<81)
+	{
+		if(board[i] != 0)
+		{
+			printf("pos : %d\n",i);
+			char x[2];
+			
+			x[0] = board[i] + 48;
+			x[1] = '\0';
+			
+			char *target = malloc(sizeof(char)*30);
+
+			strcat(target,"data/num_");
+			strcat(target,x);
+			strcat(target,".png");
+			printf("target :%s\n",target);
+			
+			surface_i = IMG_Load(target);
+			
+			dstrect.x = pos[i%9];
+			dstrect.y = pos[i/9];
+			
+
+			printf("pos x : %d pos y : %d\n",dstrect.x,dstrect.y);
+
+			SDL_BlitSurface(surface_i,NULL,surface_p,&dstrect);
+			printf("pas de segfault \n");	
+			newT = SDL_CreateTextureFromSurface(renderer,surface_p);
+			free(target);
+		}
+	i+=1;
+	}	
+				
+	return newT;
+
+}
+
+int main(int argc, char** argv)
+{
+    // Checks the number of arguments.
+    if (argc != 2)
+        errx(EXIT_FAILURE, "Usage: image-file");
+
+    // TODO:
+    // - Initialize the SDL.
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+
+    // - Create a window.
+    SDL_Window* window = SDL_CreateWindow("Image", 0, 0,0,0,
+            SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+
+    if (window == NULL)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+
+    // - Create a renderer.
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (renderer == NULL)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+
+
+
+    // - Create a texture from the image.
+    SDL_Texture* texture = IMG_LoadTexture(renderer, argv[1]);
+    if(texture == NULL)
+        errx(EXIT_FAILURE, "%s",SDL_GetError());
+
+    // Gets the width and the height of the texture.
+    int w, h;
+    if (SDL_QueryTexture(texture, NULL, NULL, &w, &h) != 0)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+
+    // TODO:
+    // - Resize the window according to the size of the image.
+    SDL_SetWindowSize(window, w, h);
+
+	SDL_Surface *surface = IMG_Load("/data/blank_grid.png");
+	texture = change_board(grille,state,surface,renderer);
+  	
+
+	if(state == 2)
+	{
+		SDL_SaveBMP(surface,"grille_g.bmp");
+	}
+	else
+	{
+		SDL_SaveBMP(surface,"grille.bmp");
+	}
+
+
+    // - Dispatch the events.
+    event_loop(renderer, texture);
+
+
+
+    // - Destroy the objects.
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
--- interface/interface.c
+++ interface/interface.c
@@ -1,9 +1,15 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 GtkWidget *window;
-GtkLabel *label;
+GtkWidget *window1;
+GtkWidget *label;
 GtkWidget *file1;
+GtkWidget *image;
+GdkPixbuf *pixbuf;
+int state = 1;
+
 
 
 
@@ -12,15 +18,15 @@ int main (int argc, char *argv[])
 	//Initialize gtk
 	gtk_init(&argc,&argv);
 
-	
 	GtkBuilder *builder;
-
+	
 	builder = gtk_builder_new();
 	
 	gtk_builder_add_from_file(builder,"inter.glade", NULL); //load our ui file
 	
 	window = GTK_WIDGET(gtk_builder_get_object(builder,"MyWindow"));
-	label = GTK_LABEL(gtk_builder_get_object(builder,"MyLabel"));
+
+	
 
 	gtk_builder_connect_signals(builder,NULL);
 
@@ -60,19 +66,26 @@ void button_cliked()
 void button_label()
 {
 	GtkBuilder *builder;
+	gtk_widget_hide(window);
 
 	builder = gtk_builder_new();
 
 	gtk_builder_add_from_file(builder,"inter.glade",NULL);
 
-	window = GTK_WIDGET(gtk_builder_get_object(builder,"Main_theme"));
+	window1 = GTK_WIDGET(gtk_builder_get_object(builder,"Main_theme"));
 	file1 = GTK_WIDGET(gtk_builder_get_object(builder,"file1"));
+	image = GTK_WIDGET(gtk_builder_get_object(builder,"image_grille"));
+	
+	
 
 	gtk_builder_connect_signals(builder,NULL);
 
 	g_object_unref(builder);
 
-	gtk_widget_show_all(window);
+	gtk_widget_show_all(window1);
+
+
+
 
 }
 
@@ -80,4 +93,24 @@ char *on_file1_file_set(GtkFileChooserButton *f)
 {
 	char *res = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(f));
 	printf("filename : %s\n",res);
+	pixbuf = gdk_pixbuf_new_from_file(res,NULL);
+	gdk_pixbuf_scale_simple(pixbuf,50,50,GDK_INTERP_BILINEAR);
+	gtk_image_set_from_pixbuf(GTK_IMAGE(image),pixbuf);
+
+	return res;
+}
+
+void button_next()
+{
+	if(state == 1)
+	{
+		state += 1;
+		//afficher grille pas completé
+	}
+	if(state == 2)
+	{
+		state += 1;
+		//afficher grille completé
+	}
+
 }
