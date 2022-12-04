f4cfaee6780a61920ae910435b9857d6a82f2af5
panpanDKC <97619055+panpanDKC@users.noreply.github.com>
2022-12-04 16:32:24 +0100

    adding few of the data set

--- neural_network/dataset/datacode.py
+++ neural_network/dataset/datacode.py
@@ -2,14 +2,14 @@ from PIL import Image, ImageDraw, ImageFont
 
 for i in range(10):
     # create image of color 'color' and of size (99, 97)
-    img = Image.new('RGB', (15, 15), color = 'white')
+    img = Image.new('RGB', (25, 25), color = 'white')
 
     # loads the font to use on text
-    fnt = ImageFont.truetype('/home/clery.pelvillain/Downloads/Adventuro.ttf', 80)
+    fnt = ImageFont.truetype('fonts/Vogue.ttf', 30)
 
     # draw on the image
     d = ImageDraw.Draw(img)
-    d.text((75, 75), str(i), font = fnt, anchor = 'mm', fill = 'black')
+    d.text((12, 12), str(i), font = fnt, anchor = 'mm', fill = 'black')
 
-    img.save("num_" + str(i) + ".png")
+    img.save("num_vogue_" + str(i) + ".png")
 
