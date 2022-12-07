27409516988fa1691d3b0cfb512e6712237de33f
Clery Pelvillain <clery.pelvillain@epita.fr>
2022-12-07 21:48:36 +0100

    restructing

--- neural_network/neronXOR.h
+++ neural_network/neronXOR.h
@@ -4,4 +4,13 @@
 double sig(double z);
 double Dsig(double z);
 void mix(int *array, size_t n);
-void proceed(int limit);
+void proceed(int limit,char* num);
+Uint32 get_pixel2(SDL_Surface *surface, int x, int y);
+double* create_ar(SDL_Surface *surface);
+int get_max(double* arr);
+double randfrom(double min, double max);
+double** get_trainset(char* num);
+void set_scratch();
+
+int forward(double* input);
+
